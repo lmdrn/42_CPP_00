836e0f4bdb8e37b92cb902b01e61348f9eafb606
lmdrn <leamedrano@gmail.com>
2024-02-09 12:58:28 +0100

    Makefile + TODO

---
+++ ex02/Account.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmedrano <lmedrano@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 12:44:03 by lmedrano          #+#    #+#             */
+/*   Updated: 2024/02/09 12:57:52 by lmedrano         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+//TODO: Create account with date + index + amount + "created"
+//[19920104_091532] index:0;amount:42;created
+
+//TODO: Display all account + all total + all deposits + all withdrawals
+//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+
+//TODO: Display all accoutns one by one with date + index + amount + deposists + withdrawals
+//[19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
+
+//TODO: Make deposist + display res with date + index + prev amount + current amount + deposit number
+//[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+
+//TODO: Display all account + all total + all deposits + all withdrawals
+//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+
+//TODO: Display all accoutns one by one with date + index + amount + deposists + withdrawals
+//[19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
+
+//TODO: Make widthdrawal + display res with date + index + prev amount + current amount + deposit number
+//[19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+
+//TODO: Display all account + all total + all deposits + all withdrawals
+//[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+
+//TODO: Display all accoutns one by one with date + index + amount + deposists + withdrawals
+//[19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
+
+//TODO: Close account with date + index + amount + "closed"
+//[19920104_091532] index:0;amount:42;closed
